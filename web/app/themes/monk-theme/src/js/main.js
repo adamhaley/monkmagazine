f9026e7b67e0d7c3c5808f9e4db4deb9cdf860b5
Adam Haley <adamhaley@gmail.com>
2023-08-05 12:12:41 -0700

    Hamburger menu transition to "x" math trig approach

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -2,3 +2,11 @@ require('../styl/reset.styl');
 require('../styl/main.styl');
 
 console.log('Hello World from main.js!');
+
+
+//hamburger nav
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function() {
+	this.classList.toggle('active');
+});
