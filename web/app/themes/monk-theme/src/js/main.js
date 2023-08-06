76a53bb91284d3a60fd32cf58210b37c0721284b
Adam Haley <adamhaley@gmail.com>
2023-08-05 20:03:17 -0700

    adde nav menu open and close , adjusted styles, added "more" image

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -6,7 +6,10 @@ console.log('Hello World from main.js!');
 
 //hamburger nav
 const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('ul.nav-menu');
 
 hamburger.addEventListener('click', function() {
 	this.classList.toggle('active');
+	nav.classList.toggle('active');
 });
+
