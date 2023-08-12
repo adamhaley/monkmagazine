7bc928f3cd81adf9e518d02c559029508e3188c7
Adam Haley <adamhaley@gmail.com>
2023-08-12 11:49:51 -0700

    added symlink to monk-theme from project root dir, refactored open/close nav into functions, calling on nav link click as well as hamburger click

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -8,23 +8,38 @@ console.log('Hello World from main.js!');
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('ul.nav-menu');
 
-hamburger.addEventListener('click', function() {
+
+function openNav() {
+	hamburger.classList.add('active'); //hamburger add active
+	nav.classList.add('displayed');
+	setTimeout(function() {
+		nav.classList.add('active');
+	}, 10);
+}
+
+function closeNav() {
+	hamburger.classList.remove('active');//hamburger remove active
+	nav.classList.remove('active');
+	nav.addEventListener('transitionend', function() {
+		nav.classList.remove('displayed'); //wait for transition to finish to remove nav from the flow
+	},{
+		capture: false,
+		once: true,
+		passive: false
+	});
+}
+
+nav.addEventListener('click', function(e) {
+	if(e.target.tagName === 'A') {
+		closeNav();
+	}
+});
+
+	hamburger.addEventListener('click', function() {
 	if(this.classList.contains('active')) {
-		this.classList.remove('active');//hamburger remove active
-		nav.classList.remove('active');
-		nav.addEventListener('transitionend', function() {
-			nav.classList.remove('displayed'); //wait for transition to finish to remove nav from the flow
-		},{
-			capture: false,
-			once: true,
-			passive: false
-		});
+		closeNav();
 	} else {
-		this.classList.add('active'); //hamburger add active
-		nav.classList.add('displayed');
-		setTimeout(function() {
-			nav.classList.add('active');
-		}, 10);
+		openNav();
 	}
 });
 
