0ad65ca4583931cfa17a22bd46b04a853a83fb80
Adam Haley <adamhaley@gmail.com>
2023-08-13 19:21:00 -0700

    added detectResponsiveEnvironment that adds body class based on standard breakpoints

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -14,6 +14,51 @@ const nav = document.querySelector('ul.nav-menu');
  * Functions
  */
 
+function detectResponsiveEnvironment() {
+
+	let breakpoints = {
+		'xs': 0,
+		'sm': 576,
+		'md': 768,
+		'lg': 992,
+		'xl': 1200
+	};
+
+	let currentBreakpoint = 'xs';
+
+	function getCurrentBreakpoint() {
+		let width = window.innerWidth;
+		console.log(width + 'px');
+		let breakpoint = 'xs';
+		for(let key in breakpoints) {
+			if(width >= breakpoints[key]) {
+				breakpoint = key;
+			}
+		}
+
+		console.log('currentBreakpoint: ' + breakpoint);
+		return breakpoint;
+	}
+	//add class to the body tag based on breakpoint
+	function updateBodyClass() {
+		let breakpoint = getCurrentBreakpoint();
+		document.body.classList.remove(['xs', 'sm', 'md', 'lg', 'xl']);
+
+		document.body.classList.forEach(function(item) {
+			if(item == breakpoint && !document.body.classList.contains(breakpoint)) {
+				document.body.classList.add(item);
+			}else{
+				document.body.classList.remove(item);
+			}
+
+		});
+		document.body.classList.add(breakpoint);
+	}
+
+	updateBodyClass()
+
+};
+
 /**
  * Open Nav
  */
@@ -64,3 +109,16 @@ hamburger.addEventListener('click', function() {
 		openNav();
 	}
 });
+
+(function() {
+	$(document).ready(detectResponsiveEnvironment);
+
+	var timeId = null;
+	function startResize() {
+		clearTimeout(timeId);
+		timeId = setTimeout(detectResponsiveEnvironment, 500);
+	}
+
+	window.addEventListener('resize', startResize);
+}).call(this);
+
