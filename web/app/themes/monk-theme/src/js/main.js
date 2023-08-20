f0db680ab006ff01719a3d4eeb928d26f3f535da
Adam Haley <adamhaley@gmail.com>
2023-08-20 14:28:17 -0700

    Added "more thrills" expandable content at the bottom

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -133,6 +133,7 @@ const nav = document.querySelector('ul.nav-menu');
 const backToTop = document.querySelector('.back-to-top a');
 const readMore = document.querySelector('.more-link');
 const moreStuff = document.querySelector('.more-stuff-to-buy');
+const moreThrills = document.querySelector('.more-thrills');
 
 /**
  * Close nav when clicking a link on it
@@ -178,6 +179,17 @@ moreStuff.addEventListener('click', function(e) {
 	e.preventDefault();
 	//add expanded class to .overflow
 	let overflow = document.querySelector('.buy-overflow');
+	if(overflow.classList.contains('in')) {
+		overflow.classList.remove('in');
+	}else{
+		overflow.classList.add('in');
+	}
+});
+
+moreThrills.addEventListener('click', function(e) {
+	e.preventDefault();
+	//add expanded class to .overflow
+	let overflow = document.querySelector('.thrills-overflow');
 	if(overflow.classList.contains('in')) {
 		overflow.classList.remove('in');
 	}else{
