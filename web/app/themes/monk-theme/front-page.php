b34fe7545f98c185a07e9d80c769e31b3c98bb4c
Adam Haley <adamhaley@gmail.com>
2023-08-30 19:32:55 -0700

    added theme php files, enqueued js and css

--- web/app/themes/monk-theme/front-page.php
+++ web/app/themes/monk-theme/front-page.php
@@ -7,15 +7,248 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Monk Magazine</title>
 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+
 	<?php wp_head(); ?>
-
-	<link rel="stylesheet" href="./assets/css/main.css">
-	<!--local css file-->
-
-	<script src="assets/bundle.js"></script>
-
 </head>
 <body>
+<div class="gutter">
 
+</div>
+<header>
+
+	<img class="header" src="assets/images/monk-magazine.jpg" alt="Monk Magazine">
+</header>
+<div class="gutter">
+	<div class="hamburger">
+		<img class="menu" src="assets/images/menu.svg" alt="">
+		<img class="close" src="assets/mages/x.svg" alt="">
+	</div>
+</div>
+
+
+<div class="container">
+	<!-- simple mobile true image -->
+	<div class="row">
+		<img class="simple-mobile-true" src="assets/images/simple-mobile-true.png" alt="Simple.Mobile.True." >
+	</div><br />
+	<!-- main image carousel -->
+	<div class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="assets/images/monks-home.png" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<!--place kitten -->
+				<img class="d-block w-100" src="https://placekitten.com/g/100/300" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="https://placekitten.com/g/100/300" alt="Third slide">
+			</div>
+		</div>
+	</div>
+	<div class="content">
+		<img class="read-this" src="assets/images/read-this.png" alt="Read This" /><br />
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+		Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliqua.
+
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at deserunt dicta dignissimos doloremque ducimus eius error expedita fuga inventore ipsa iste labore libero nisi pariatur quidem sed tempora, velit!
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores aspernatur blanditiis cumque distinctio dolores ex in maxime modi nisi non placeat, quae unde voluptate voluptatem? Enim exercitationem ipsam reprehenderit.
+
+		<a name="read-this"></a>
+		<a href="" class="more-link"><img src="assets/images/more.png" alt="More" class="more" /><i class="fa-solid fa-plus"></i>
+		</a>
+	</div>
+	<div class="row">
+			<span class="overflow">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /><br />A deserunt, esse eum excepturi, laborum mollitia nemo nobis non odio officia omnis quam quia quibusdam quo repudiandae sed, tempora vel voluptatum!
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos dolorem doloremque est, laborum minus molestias omnis perferendis quae. Ducimus inventore ipsa voluptatem? A quidem reiciendis tenetur. Doloremque, quae suscipit!
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, amet at blanditiis consequatur cum dicta dolor esse eum inventore iusto nemo nostrum obcaecati possimus quidem quo, rerum similique veniam.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aliquid amet est expedita laborum minima modi voluptatibus. Delectus deleniti deserunt facilis ipsum magni placeat quae quo rem suscipit, vel?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquid assumenda dolore magni pariatur quidem saepe sequi totam, voluptate. At distinctio error ex facilis incidunt laboriosam ratione sequi, sint?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. At optio, quo. Aliquam deleniti deserunt dolorem eum exercitationem iste itaque iure natus nostrum placeat quibusdam quo, quos ratione tempore tenetur veritatis.
+				<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, amet at blanditiis consequatur cum dicta dolor esse eum inventore iusto nemo nostrum obcaecati possimus quidem quo, rerum similique veniam.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aliquid amet est expedita laborum minima modi voluptatibus. Delectus deleniti deserunt facilis ipsum magni placeat quae quo rem suscipit, vel?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquid assumenda dolore magni pariatur quidem saepe sequi totam, voluptate. At distinctio error ex facilis incidunt laboriosam ratione sequi, sint?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. At optio, quo. Aliquam deleniti deserunt dolorem eum exercitationem iste itaque iure natus nostrum placeat quibusdam quo, quos ratione tempore tenetur veritatis.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere iure iusto quidem repellendus voluptate! At, eius sapiente? Aut beatae expedita fuga laudantium minus nobis nulla pariatur quasi reiciendis rem, voluptas?			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere iure iusto quidem repellendus voluptate! At, eius sapiente? Aut beatae expedita fuga laudantium minus nobis nulla pariatur quasi reiciendis rem, voluptas?
+				<div class="overflow-bottom">
+					<i class="close-icon fa fa-minus"></i>
+				</div>
+			</span>
+
+	</div>
+	<div class="buy">
+		<img src="assets/images/literally-buy.png" alt="Buy" />
+	</div>
+
+	<a href="" class="cart"><i class="fa-solid fa-shopping-cart"></i></a>
+	<ul class="back-issues">
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #4"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #5"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #6"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #7"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #8"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #9"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #10"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #11"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #12"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #13"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #14"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #15"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #16"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #17"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #18"></li>
+		<li class="issue"><img src="https://placekitten.com/g/200/300" alt="Issue #19"></li>
+	</ul>
+	<a href="" class="more-stuff-to-buy"><img src="assets/images/more-stuff-to-buy.png" alt="More Stuff To Buy" class="more" /><i class="fa-solid fa-plus"></i></a>
+	<div class="buy-overflow overflow">
+		<ul class="more-stuff">
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #4"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #4"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #4"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #5"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #6"></li>
+			<li class="product"><img src="https://placekitten.com/g/201/300" alt="Issue #7"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #8"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #9"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #10"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #11"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #12"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #13"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #14"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #15"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #16"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #17"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #18"></li>
+			<li class="product"><img src="https://placekitten.com/g/200/300" alt="Issue #19"></li>
+		</ul>
+		<div class="overflow-bottom">
+			<i class="close-icon fa fa-minus"></i>
+		</div>
+	</div>
+	<div class="on-the-road">
+		<div class="on-the-road-content">
+			<p>
+				<a name="on-the-road"></a>
+				<img src="assets/images/on-the-road-with-the-monks.png" alt="On The Road With The Monks" class="on-the-road-with-the-monks" /><br />
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+
+			<br />
+			<img src="assets/images/on-the-road.jpg" alt="Monk On The Road" class="monk-on-the-road" />
+			<a href="" class="more-thrills"><img src="assets/images/more-thrills.png" alt="More Thrills" class="more-thrills" /><i class="fa-solid fa-plus"></i></a>
+		</div>
+	</div>
+	<div class="thrills-overflow overflow">
+		<div class="thrills-wrapper">
+			<!--three columns-->
+			<div class="thrill">
+				<img src="https://placekitten.com/g/200/200" alt="Issue #4" />
+				<br />
+				<p class="thrill-content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+					<span class="extended">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+							</span>
+				</p>
+			</div>
+			<div class="thrill">
+				<img src="https://placekitten.com/g/200/200" alt="Issue #5" />
+				<br />
+				<p class="thrill-content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+					<span class="extended">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+							</span>
+
+				</p>
+			</div>
+			<div class="thrill">
+				<img src="https://placekitten.com/g/200/200" alt="Issue #6" />
+				<br />
+				<p class="thrill-content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+					<span class="extended">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia quibusdam sed tempora tenetur vel vitae voluptatum! Laudantium quaerat quo voluptas.
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque doloremque dolorum et, ipsa laboriosam, libero modi odit officia.
+							</span>
+				</p>
+			</div>
+		</div>
+		<div class="overflow-bottom">
+			<i class="close-icon fa fa-minus"></i>
+		</div>
+	</div>
+</div>
+<div class="footer">
+	<img src="assets/images/connect.png" alt="Footer" class="connect" />
+
+	<br />
+	<div class="social">
+		<!-- inline SVG gloriousness -->
+		<!--facebook-->
+		<svg class="facebook" width="20px" height="20px" viewBox="0 0 20 20" aria-hidden="true">
+			<path d="M20,10.1c0-5.5-4.5-10-10-10S0,4.5,0,10.1c0,5,3.7,9.1,8.4,9.9v-7H5.9v-2.9h2.5V7.9C8.4,5.4,9.9,4,12.2,4c1.1,0,2.2,0.2,2.2,0.2v2.5h-1.3c-1.2,0-1.6,0.8-1.6,1.6v1.9h2.8L13.9,13h-2.3v7C16.3,19.2,20,15.1,20,10.1z"></path>
+		</svg>
+		<!--twitter-->
+		<svg class="twitter" width="20px" height="20px" viewBox="0 0 20 20" aria-hidden="true"><path d="M20,3.8c-0.7,0.3-1.5,0.5-2.4,0.6c0.8-0.5,1.5-1.3,1.8-2.3c-0.8,0.5-1.7,0.8-2.6,1c-0.7-0.8-1.8-1.3-3-1.3c-2.3,0-4.1,1.8-4.1,4.1c0,0.3,0,0.6,0.1,0.9C6.4,6.7,3.4,5.1,1.4,2.6C1,3.2,0.8,3.9,0.8,4.7c0,1.4,0.7,2.7,1.8,3.4C2,8.1,1.4,7.9,0.8,7.6c0,0,0,0,0,0.1c0,2,1.4,3.6,3.3,4c-0.3,0.1-0.7,0.1-1.1,0.1c-0.3,0-0.5,0-0.8-0.1c0.5,1.6,2,2.8,3.8,2.8c-1.4,1.1-3.2,1.8-5.1,1.8c-0.3,0-0.7,0-1-0.1c1.8,1.2,4,1.8,6.3,1.8c7.5,0,11.7-6.3,11.7-11.7c0-0.2,0-0.4,0-0.5C18.8,5.3,19.4,4.6,20,3.8z"></path></svg>
+		<!--ig svg -->
+		<svg class="ig" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+			<circle cx="10" cy="10" r="3.3"></circle>
+			<path d="M14.2,0H5.8C2.6,0,0,2.6,0,5.8v8.3C0,17.4,2.6,20,5.8,20h8.3c3.2,0,5.8-2.6,5.8-5.8V5.8C20,2.6,17.4,0,14.2,0zM10,15c-2.8,0-5-2.2-5-5s2.2-5,5-5s5,2.2,5,5S12.8,15,10,15z M15.8,5C15.4,5,15,4.6,15,4.2s0.4-0.8,0.8-0.8s0.8,0.4,0.8,0.8S16.3,5,15.8,5z"></path>
+		</svg>
+		<!--email svg -->
+		<svg class="email" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><path d="M10,10.1L0,4.7C0.1,3.2,1.4,2,3,2h14c1.6,0,2.9,1.2,3,2.8L10,10.1z M10,11.8c-0.1,0-0.2,0-0.4-0.1L0,6.4V15c0,1.7,1.3,3,3,3h4.9h4.3H17c1.7,0,3-1.3,3-3V6.4l-9.6,5.2C10.2,11.7,10.1,11.7,10,11.8z"></path></svg>
+
+		<!-- phone svg -->
+		<svg class="phone" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+			<path d="M4.8,0C2.1,0,0,2.1,0,4.8v10.5C0,17.9,2.1,20,4.8,20h10.5c2.6,0,4.8-2.1,4.8-4.8V4.8C20,2.1,17.9,0,15.2,0H4.8z M6.7,3.8C7,3.8,7.2,4,7.4,4.3C7.6,4.6,7.9,5,8.3,5.6c0.3,0.5,0.4,1.2,0.1,1.8l-0.7,1C7.4,8.7,7.4,9,7.5,9.3c0.2,0.5,0.6,1.2,1.3,1.9c0.7,0.7,1.4,1.1,1.9,1.3c0.3,0.1,0.6,0.1,0.9-0.1l1-0.7c0.6-0.3,1.3-0.3,1.8,0.1c0.6,0.4,1.1,0.7,1.3,0.9c0.3,0.2,0.4,0.4,0.4,0.7c0.1,1.7-1.2,2.4-1.6,2.4c-0.3,0-3.4,0.4-7-3.2s-3.2-6.8-3.2-7C4.3,5.1,5,3.8,6.7,3.8z"></path>
+		</svg>
+	</div>
+	<div class="copyright"> &copy;2023 Monk Magazine</div>
+</div>
+
+
+<!--NAV MENU-->
+<ul class="nav-menu">
+	<li><a href="#read-this">Read This</a></li>
+	<li><a href="#buy">Buy</a></li>
+	<li><a href="#on-the-road">On The Road</a></li>
+	<li><a href="#cart">Shopping Cart</a></li>
+	<div class="social">
+		<!-- inline SVG gloriousness -->
+		<!--facebook-->
+		<svg class="facebook" width="20px" height="20px" viewBox="0 0 20 20" aria-hidden="true">
+			<path d="M20,10.1c0-5.5-4.5-10-10-10S0,4.5,0,10.1c0,5,3.7,9.1,8.4,9.9v-7H5.9v-2.9h2.5V7.9C8.4,5.4,9.9,4,12.2,4c1.1,0,2.2,0.2,2.2,0.2v2.5h-1.3c-1.2,0-1.6,0.8-1.6,1.6v1.9h2.8L13.9,13h-2.3v7C16.3,19.2,20,15.1,20,10.1z"></path>
+		</svg>
+		<!--twitter-->
+		<svg class="twitter" width="20px" height="20px" viewBox="0 0 20 20" aria-hidden="true"><path d="M20,3.8c-0.7,0.3-1.5,0.5-2.4,0.6c0.8-0.5,1.5-1.3,1.8-2.3c-0.8,0.5-1.7,0.8-2.6,1c-0.7-0.8-1.8-1.3-3-1.3c-2.3,0-4.1,1.8-4.1,4.1c0,0.3,0,0.6,0.1,0.9C6.4,6.7,3.4,5.1,1.4,2.6C1,3.2,0.8,3.9,0.8,4.7c0,1.4,0.7,2.7,1.8,3.4C2,8.1,1.4,7.9,0.8,7.6c0,0,0,0,0,0.1c0,2,1.4,3.6,3.3,4c-0.3,0.1-0.7,0.1-1.1,0.1c-0.3,0-0.5,0-0.8-0.1c0.5,1.6,2,2.8,3.8,2.8c-1.4,1.1-3.2,1.8-5.1,1.8c-0.3,0-0.7,0-1-0.1c1.8,1.2,4,1.8,6.3,1.8c7.5,0,11.7-6.3,11.7-11.7c0-0.2,0-0.4,0-0.5C18.8,5.3,19.4,4.6,20,3.8z"></path></svg>
+		<!--ig svg -->
+		<svg class="ig" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+			<circle cx="10" cy="10" r="3.3"></circle>
+			<path d="M14.2,0H5.8C2.6,0,0,2.6,0,5.8v8.3C0,17.4,2.6,20,5.8,20h8.3c3.2,0,5.8-2.6,5.8-5.8V5.8C20,2.6,17.4,0,14.2,0zM10,15c-2.8,0-5-2.2-5-5s2.2-5,5-5s5,2.2,5,5S12.8,15,10,15z M15.8,5C15.4,5,15,4.6,15,4.2s0.4-0.8,0.8-0.8s0.8,0.4,0.8,0.8S16.3,5,15.8,5z"></path>
+		</svg>
+		<!--email svg -->
+		<svg class="email" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true"><path d="M10,10.1L0,4.7C0.1,3.2,1.4,2,3,2h14c1.6,0,2.9,1.2,3,2.8L10,10.1z M10,11.8c-0.1,0-0.2,0-0.4-0.1L0,6.4V15c0,1.7,1.3,3,3,3h4.9h4.3H17c1.7,0,3-1.3,3-3V6.4l-9.6,5.2C10.2,11.7,10.1,11.7,10,11.8z"></path></svg>
+
+		<!-- phone svg -->
+		<svg class="phone" width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+			<path d="M4.8,0C2.1,0,0,2.1,0,4.8v10.5C0,17.9,2.1,20,4.8,20h10.5c2.6,0,4.8-2.1,4.8-4.8V4.8C20,2.1,17.9,0,15.2,0H4.8z M6.7,3.8C7,3.8,7.2,4,7.4,4.3C7.6,4.6,7.9,5,8.3,5.6c0.3,0.5,0.4,1.2,0.1,1.8l-0.7,1C7.4,8.7,7.4,9,7.5,9.3c0.2,0.5,0.6,1.2,1.3,1.9c0.7,0.7,1.4,1.1,1.9,1.3c0.3,0.1,0.6,0.1,0.9-0.1l1-0.7c0.6-0.3,1.3-0.3,1.8,0.1c0.6,0.4,1.1,0.7,1.3,0.9c0.3,0.2,0.4,0.4,0.4,0.7c0.1,1.7-1.2,2.4-1.6,2.4c-0.3,0-3.4,0.4-7-3.2s-3.2-6.8-3.2-7C4.3,5.1,5,3.8,6.7,3.8z"></path>
+		</svg>
+	</div>
+</ul>
+<div class="back-to-top">
+	<a href="#">
+		<svg class="ct-icon" width="15" height="15" viewBox="0 0 20 20"><path d="M10,0L9.4,0.6L0.8,9.1l1.2,1.2l7.1-7.1V20h1.7V3.3l7.1,7.1l1.2-1.2l-8.5-8.5L10,0z"></path></svg>
+	</a>
+</div>
+<!--END NAV MENU-->
+<?php wp_footer();?>
 </body>
 </html>
