323496c68c4c2c970587c4b476a4c09430458b22
Adam Haley <adamhaley@gmail.com>
2023-09-03 17:33:45 -0700

    commenting links to full cart pages till fix cart layout

--- web/app/themes/monk-theme/front-page.php
+++ web/app/themes/monk-theme/front-page.php
@@ -91,7 +91,7 @@
 			$loop = new WP_Query( $args );
 				if ( $loop->have_posts() ) {
 				while ( $loop->have_posts() ) : $loop->the_post();
-				echo "<li class='issue'><a href='" . get_permalink() . "'><img src='" . get_the_post_thumbnail_url() . "' alt='" . get_the_title() . "' /></a></li>";
+				echo "<li class='issue'><!--<a href='" . get_permalink() . "'>--><img src='" . get_the_post_thumbnail_url() . "' alt='" . get_the_title() . "' /><!--</a>--></li>";
 				endwhile;
 			} else {
 				echo __( 'No products found' );
@@ -114,7 +114,7 @@
 			$loop = new WP_Query( $args );
 			if ( $loop->have_posts() ) {
 				while ( $loop->have_posts() ) : $loop->the_post();
-					echo "<li class='product'><a href='" . get_permalink() . "'><img src='" . get_the_post_thumbnail_url() . "' alt='" . get_the_title() . "' /></a></li>";
+					echo "<li class='product'><!--<a href='" . get_permalink() . "'>--><img src='" . get_the_post_thumbnail_url() . "' alt='" . get_the_title() . "' /><!--</a>--></li>";
 				endwhile;
 			} else {
 				echo __( 'No products found' );
