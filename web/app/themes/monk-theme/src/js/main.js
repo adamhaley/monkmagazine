253941edf0e17b55236fa6e1ce7151aaed7175e7
Adam Haley <adamhaley@gmail.com>
2023-09-10 00:35:41 -0700

    attaching facebook and instagram links to social navs

--- web/app/themes/monk-theme/src/js/main.js
+++ web/app/themes/monk-theme/src/js/main.js
@@ -5,8 +5,6 @@ require('../styl/main.styl');
 
 console.log('Hello World from main.js!');
 
-
-
 /**
  * Functions
  */
@@ -95,6 +93,9 @@ function openNav() {
 	setTimeout(function() {
 		nav.classList.add('active');
 	}, 10);
+
+	//attach social link events
+	attachSocialLinks();
 }
 
 /**
@@ -139,6 +140,29 @@ const readMore = document.querySelector('.more-link');
 const moreStuff = document.querySelector('.more-stuff-to-buy');
 const moreThrills = document.querySelector('.more-thrills');
 const overflowBottom = document.querySelectorAll('.overflow-bottom');
+
+//social links
+function attachSocialLinks() {
+	const facebookLinks = document.querySelectorAll('.facebook');
+	const instagramLinks = document.querySelectorAll('.ig');
+
+	facebookLinks.forEach(function(link) {
+		link.addEventListener('click', function(e) {
+			e.preventDefault();
+			window.open('https://www.facebook.com/people/Monk-Magazine/100067147688530/', '_blank');
+		});
+	});
+
+	instagramLinks.forEach(function(link) {
+		link.addEventListener('click', function(e) {
+			e.preventDefault();
+			window.open('https://www.instagram.com/monk.magazine/', '_blank');
+		});
+	});
+
+}
+
+
 
 /**
  * Close nav when clicking a link on it
