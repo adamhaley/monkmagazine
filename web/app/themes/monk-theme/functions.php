7f11ecafa3bb89f982ef9472a406c9ca758ff83a
Adam Haley <adamhaley@gmail.com>
2023-09-11 18:18:11 -0700

    updated single-product template override, moved woo commerce support action

--- web/app/themes/monk-theme/functions.php
+++ web/app/themes/monk-theme/functions.php
@@ -69,7 +69,6 @@ function add_woo_support()
 	add_theme_support( 'woocommerce' );
 }
 
-add_action('after_setup_theme', 'add_woo_support');
 
 add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
 add_action('wp_enqueue_scripts', 'monktheme_register_styles');
@@ -80,3 +79,4 @@ add_action('woocommerce_before_checkout_form','add_open_container_div', 9);
 add_action('woocommerce_after_checkout_form','add_close_container_div', 170);
 add_action('woocommerce_before_cart','add_open_container_div', 10);
 add_action('woocommerce_after_cart','add_close_container_div', 200);
+add_action('after_setup_theme', 'add_woo_support');
