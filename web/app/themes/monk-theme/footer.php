539cde17195218ef37725cec2828f187bb75a20b
Monk <monkmagazine@adamhaley.com>
2023-09-14 00:32:22 +0000

    Updates to shop styles, removed nav from footer.php

--- web/app/themes/monk-theme/footer.php
+++ web/app/themes/monk-theme/footer.php
@@ -26,7 +26,7 @@
 	<div class="copyright"> &copy;2023 Monk Magazine</div>
 </div>
 
-
+<?php if(is_front_page()){ ?>
 <!--NAV MENU-->
 <ul class="nav-menu">
 	<li><a href="#read-this">Read This</a></li>
@@ -55,7 +55,6 @@
 		</svg>
 	</div>
 </ul>
-<?php if(is_front_page()){ ?>
 <div class="back-to-top">
 	<a href="#">
 		<svg class="ct-icon" width="15" height="15" viewBox="0 0 20 20"><path d="M10,0L9.4,0.6L0.8,9.1l1.2,1.2l7.1-7.1V20h1.7V3.3l7.1,7.1l1.2-1.2l-8.5-8.5L10,0z"></path></svg>
