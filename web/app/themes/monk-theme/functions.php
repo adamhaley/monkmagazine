898c15bc2545c1c52f5070c7ec146cce1c380e18
Adam Haley <adamhaley@gmail.com>
2023-09-30 17:07:57 -0700

    added social links for X and Linkedin, some css on hamburger

--- web/app/themes/monk-theme/functions.php
+++ web/app/themes/monk-theme/functions.php
@@ -81,15 +81,17 @@ function add_woo_support()
 	add_theme_support( 'post-thumbnails' );
 }
 
-function monk_change_number_related_products( $args ) {
+function jk_change_number_related_products( $args ) {
 	$args['posts_per_page'] = 16; // # of related products
 	$args['columns'] = 4; // # of columns per row
 	return $args;
 }
 
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 10 );
-add_filter( 'woocommerce_product_description_heading', '__return_null' );
+add_filter('woocommerce_product_description_heading', '__return_null' );
 add_filter('use_block_editor_for_post', '__return_false');
+add_filter('woocommerce_output_related_products_args', 'jk_change_number_related_products', 20 );
+add_action('after_setup_theme', 'add_woo_support');
 add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 10);
 add_action('wp_enqueue_scripts', 'monktheme_register_styles');
 add_action('wp_enqueue_scripts', 'monktheme_register_scripts');
@@ -105,9 +107,6 @@ add_action('woocommerce_before_cart','add_header', 5);
 add_action('woocommerce_before_cart','add_open_container_div', 12);
 add_action('woocommerce_after_cart','add_close_container_div', 200);
 add_action('woocommerce_after_cart','add_footer', 215);
-add_filter( 'woocommerce_output_related_products_args', 'monk_change_number_related_products', 9999 );
-
-add_action('after_setup_theme', 'add_woo_support');
 
 remove_filter( 'woocommerce_before_checkout_form', 'wpautop', 10 );
 
